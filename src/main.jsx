4d7e31bee1fc6271ae099c8650bec1bcd144a2e3
zxu <zxu@yext.com>
2022-04-06 14:27:43 -0400

    add schema

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import Schema from './schema/schema'
+import Schema from './schema/schema'
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Schema />
-//   </React.StrictMode>,
-//   document.getElementById('jsonld')
-// )
+ReactDOM.render(
+  <React.StrictMode>
+    <Schema />
+  </React.StrictMode>,
+  document.getElementById('jsonld')
+)
 
 ReactDOM.render(
   <React.StrictMode>
--- src/schema/schema.jsx
+++ src/schema/schema.jsx
@@ -1,21 +1,21 @@
-// function Schema() {
-//   const articleStructuredData = {
-//     '@context': 'https://schema.org',
-//     '@type': 'Article',
-//     headline: 'Structured data for you',
-//     description: 'This is an article that demonstrates structured data.',
-//     image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/JSON-LD.svg',
-//     datePublished: new Date('2021-09-04T09:25:01.340Z').toISOString(),
-//     author: {
-//       '@type': 'Person',
-//       name: 'John Reilly',
-//       url: 'https://twitter.com/johnny_reilly',
-//     },
-//   };
+function Schema() {
+  const articleStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: 'Structured data for you',
+    description: 'This is an article that demonstrates structured data.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/JSON-LD.svg',
+    datePublished: new Date('2021-09-04T09:25:01.340Z').toISOString(),
+    author: {
+      '@type': 'Person',
+      name: 'John Reilly',
+      url: 'https://twitter.com/johnny_reilly',
+    },
+  };
 
-//   return (
-//     JSON.stringify(articleStructuredData)
-//   )
-// }
+  return (
+    JSON.stringify(articleStructuredData)
+  )
+}
 
-// export default Schema
+export default Schema
